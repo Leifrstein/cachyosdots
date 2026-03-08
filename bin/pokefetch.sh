f280f449dabc56b857c3aae81df8bd43e033b6f0
Lucas <lucaspc@protonmail.ch>
2026-03-08 14:58:54 -0300

    Pokefetch for fastfetch.

---
+++ bin/pokefetch.sh
@@ -0,0 +1,61 @@
+# by discomanfulanito - https://github.com/Discomanfulanito/pokefetch,
+# for everyone — as code should be
+
+# Sprites are drawn from here if not using generic random
+#POKEMON_LIST=(
+  #victini
+  #"mimikyu -s"
+  #celebi
+  #furret
+  #"mewtwo --mega-y"
+#)
+# Change with your fetcher
+FETCHER="fastfetch --logo none"
+
+FETCHER_HEIGHT=$($FETCHER | wc -l)
+
+# Extra settings
+EXTRA_PADDING_H=2
+EXTRA_PADDING_W=0
+
+# Room for the sprite
+WIDTH=30
+
+# Gets a sprite via pokeget from the POKEMON_LIST
+#sprite=$(pokeget ${POKEMON_LIST[RANDOM % ${#POKEMON_LIST[@]}]} --hide-name)
+
+# Gets a random sprite via pokeget
+sprite=$(pokeget random --hide-name)
+
+# Gets sprite's height
+height=$(echo "$sprite" | wc -l)
+
+# Pad for vertical centering
+pad_top=$((($FETCHER_HEIGHT - $height) / 2))
+pad_top=$((pad_top + EXTRA_PADDING_H))
+
+# Just for safety
+(( pad_top < 0 )) && pad_top=0
+
+# Gets sprite's sprite_width
+# strip ANSI color codes with sed to get the true printed width
+sprite_width=$(
+  printf '%s\n' "$sprite" \
+  | sed 's/\x1b\[[0-9;]*m//g' \
+  | awk '{ if (length > max) max = length } END { print max }'
+)
+
+# Calculate the lateral padding
+pad_Left=$((($WIDTH - sprite_width) / 2))
+# +1 to avoid odd-width rounding issues so logo area remains visually centered
+pad_Right=$((($WIDTH - sprite_width + 1) / 2))
+
+pad_Left=$((pad_Left + EXTRA_PADDING_W))
+pad_Right=$((pad_Right + EXTRA_PADDING_W))
+
+# Just for safety
+(( pad_Left < 0 )) && pad_Left=0
+(( pad_Right < 0 )) && pad_Right=0
+
+# this may not work for your fetcher, check them all
+echo "$sprite" | $FETCHER --file-raw - --logo-padding-top $pad_top --logo-padding-left $pad_Left --logo-padding-right $pad_Right
